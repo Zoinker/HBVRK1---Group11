3c5d5da0c03fd68e2425c2c88f11523f8416b175
Nokkvi <nos1@hi.is>
2016-11-03 15:54:11 +0000

    Fix á template vandræðum

--- SkreppIs/passenger/views.py
+++ SkreppIs/passenger/views.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 
 def index(request):
-    context = " "
-    return render(request, 'passenger/index.html')
+    context = {}
+    return render(request, 'passenger/index.html', context)
     #return HttpResponse("<h1>whaht</h1>")
