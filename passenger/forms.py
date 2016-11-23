4129300e02abba79e65b600f78be9212c33731f2
MrZoinker <mrzoinker@gmail.com>
2016-11-23 14:17:54 +0000

    Leitarfikt

--- passenger/forms.py
+++ passenger/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django import forms
 from driver.models import Driver
 
+
 class SearchForm(forms.ModelForm):
     zoneFrom = forms.CharField()
     zoneTo = forms.CharField()
--- passenger/views.py
+++ passenger/views.py
@@ -11,12 +11,12 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import SearchForm
 from driver.models import Driver
+import logging
 
 def index(request):
     context = {}
     return render(request, 'passenger/index.html', context)
     #return HttpResponse("<h1>whaht</h1>")
-
 
 
 class SearchFormView(View):
@@ -28,11 +28,17 @@ class SearchFormView(View):
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        form = self.form_class(request.GET)
-        found_entries = Driver.objects.filter(zoneFrom="\"zoneFrom\"").filter(zoneTo="\"zoneTo\"")
 
-        context = {'found_entries':found_entries}
-        return render(request, 'passenger/search_results.html', {'form': form}, context)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            zone_from = form.cleaned_data['zoneFrom']
+            zone_to = form.cleaned_data['zoneTo']
+            logging.warning(zone_to)
+            found_entries = Driver.objects.filter(zoneFrom=zone_from).filter(zoneTo=zone_to)
+
+            context = {'found_entries':found_entries}
+            logging.warning(context)
+            return render(request, 'passenger/search_results.html', {'form': form}, context)
 
 
 
