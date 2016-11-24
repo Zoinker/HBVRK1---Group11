f15f6273717d289f6d02aaa8211ccdd49fe95f7e
Nokkvi <nos1@hi.is>
2016-11-24 02:43:19 +0000

    Praise Jesus!

--- passenger/forms.py
+++ passenger/forms.py
@@ -1,12 +1,27 @@
 from django.contrib.auth.models import User
 from django import forms
-from driver.models import Driver
+from driver.models import Driver, Zone
 
+LOCATIONS = (
+    ('Breiðholt', 'Breiðholt'),
+    ('Washington', 'Washington'),
+    ('Moskva', 'Moskva'),
+    ('Garðabær', 'Garðabær'),
+)
 
-class SearchForm(forms.ModelForm):
+class SarchForm(forms.ModelForm):
     zoneFrom = forms.CharField()
     zoneTo = forms.CharField()
 
     class Meta:
         model = Driver
         fields = ['zoneFrom', 'zoneTo']
+
+class SearchForm(forms.ModelForm):
+    zoneFrom = forms.ChoiceField(choices=LOCATIONS, required=True )
+    zoneTo = forms.ChoiceField(choices=LOCATIONS, required=True )
+
+    class Meta:
+        model = Driver
+        fields = ['zoneFrom', 'zoneTo']
+
