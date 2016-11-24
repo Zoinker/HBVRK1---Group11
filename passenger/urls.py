10270d4b27abcdb7dc0261b1b358e7f6611d39aa
Nokkvi <nos1@hi.is>
2016-11-24 01:03:44 +0000

    1

--- passenger/urls.py
+++ passenger/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.SearchFormView.as_view(), name='index'),
-    url(r'^$', v)
     #url(r'^upgrade/$', views.becomeDriver.as_view(), name='upgrade'),
     #url(r'^logout/$', views.logout.as_view(), name='logout'),
 ]
