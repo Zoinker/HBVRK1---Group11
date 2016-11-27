8a8c7c134edb524bf288bfcb751a67329da82a28
Nokkvi <nos1@hi.is>
2016-11-27 04:51:16 +0000

    allt auka dót farið úr /driver

--- driver/forms.py
+++ driver/forms.py
@@ -1,21 +1,10 @@
 from django.contrib.auth.models import User
 from django import forms
 from driver.models import Driver, Zone
-LOCATIONS = (
-    ('Breiðholt', 'Breiðholt'),
-    ('Washington', 'Washington'),
-    ('Moskva', 'Moskva'),
-    ('Garðabær', 'Garðabær'),
-    ('Hafnafjörður', 'Hafnafjörður'),
-    ('Kópavogur', 'Kópavogur'),
-    ('Mosfellsbær', 'Mosfellsbær'),
-    ('Seltjarnarnes', 'Seltjarnarnes'),
-    ('Miðbær', 'Miðbær')
-)
+from django.forms.widgets import CheckboxSelectMultiple
+
 
 class setZonesForm(forms.ModelForm):
-    zone = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=LOCATIONS, required=False)
-
     class Meta:
         model = Driver
         fields = ['zones']
